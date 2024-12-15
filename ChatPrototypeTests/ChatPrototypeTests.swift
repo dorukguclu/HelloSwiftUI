44ad2213a00b75445a8a512d54e0df38affcefba
dorukguclu <doruk.guclu@hisarschool.k12.tr>
2024-12-15 18:41:17 +0300

    Initial Commit

---
+++ ChatPrototypeTests/ChatPrototypeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeTests.swift
+//  ChatPrototypeTests
+//
+//  Created by Doruk GÜÇLÜ on 15.12.2024.
+//
+
+import Testing
+@testable import ChatPrototype
+
+struct ChatPrototypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
